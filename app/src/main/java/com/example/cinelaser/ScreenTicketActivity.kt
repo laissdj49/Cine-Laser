f499a822a2a591346becb8d04a1b936152f5254c
laissdj49 <laissdj49@gmail.com>
2022-07-13 12:37:54 -0300

    initial branch

---
+++ app/src/main/java/com/example/cinelaser/ScreenTicketActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cinelaser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreenTicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_ticket)
+    }
+}
